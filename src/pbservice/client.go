e930ce1ca8244a1d65314f12eef94e1b74c21d0b
liyang <29890639+butterflyforever@users.noreply.github.com>
2020-10-14 01:57:47 -0400

    a2pb

--- src/pbservice/client.go
+++ src/pbservice/client.go
@@ -1,30 +1,30 @@
 package pbservice
 
-import "viewservice"
-import "net/rpc"
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"net/rpc"
+	"viewservice"
+)
 
 // You'll probably need to uncomment these:
 // import "time"
 // import "crypto/rand"
 // import "math/big"
 
-
-
 type Clerk struct {
-  vs *viewservice.Clerk
-  // Your declarations here
+	vs *viewservice.Clerk
+	// Your declarations here
 }
-
 
 func MakeClerk(vshost string, me string) *Clerk {
-  ck := new(Clerk)
-  ck.vs = viewservice.MakeClerk(me, vshost)
-  // Your ck.* initializations here
+	ck := new(Clerk)
+	ck.vs = viewservice.MakeClerk(me, vshost)
+	// Your ck.* initializations here
 
-  return ck
+	return ck
 }
-
 
 //
 // call() sends an RPC to the rpcname handler on server srv
@@ -43,20 +43,20 @@ func MakeClerk(vshost string, me string) *Clerk {
 // please don't change this function.
 //
 func call(srv string, rpcname string,
-          args interface{}, reply interface{}) bool {
-  c, errx := rpc.Dial("unix", srv)
-  if errx != nil {
-    return false
-  }
-  defer c.Close()
-    
-  err := c.Call(rpcname, args, reply)
-  if err == nil {
-    return true
-  }
+	args interface{}, reply interface{}) bool {
+	c, errx := rpc.Dial("unix", srv)
+	if errx != nil {
+		return false
+	}
+	defer c.Close()
 
-  fmt.Println(err)
-  return false
+	err := c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
 }
 
 //
@@ -67,10 +67,25 @@ func call(srv string, rpcname string,
 // says the key doesn't exist (has never been Put().
 //
 func (ck *Clerk) Get(key string) string {
+	args := &GetArgs{}
+	var reply GetReply
+	args.Key = key
+	args.Id = nrand()
+	ok := false
+	for ok == false {
+		vsView, _ := ck.vs.Get()
+		args.Viewnum = vsView.Viewnum
+		reply.Err = ""
+		reply.Value = ""
+		ok = call(vsView.Primary, "PBServer.Get", args, &reply)
+		if reply.Err != OK && reply.Err != ErrNoKey {
+			ok = false
+		}
+	}
 
-  // Your code here.
-
-  return "???"
+	// Your code here.
+	// If key doesn't exist, set Value = ""
+	return reply.Value
 }
 
 //
@@ -79,14 +94,43 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutExt(key string, value string, dohash bool) string {
 
-  // Your code here.
-  return "???"
+	// Your code here.
+	args := &PutArgs{}
+	var reply PutReply
+	args.Key = key
+	args.Value = value
+	args.DoHash = dohash
+	args.Id = nrand()
+	ok := false
+	for ok == false {
+		vsView, _ := ck.vs.Get()
+		args.Viewnum = vsView.Viewnum
+		reply.Err = ""
+		reply.PreviousValue = ""
+		ok = call(vsView.Primary, "PBServer.Put", args, &reply)
+		if reply.Err != OK {
+			ok = false
+		}
+	}
+	// fmt.Printf("Client %t\n", ok)
+	if dohash {
+		return reply.PreviousValue
+	} else {
+		return value
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
-  ck.PutExt(key, value, false)
+	ck.PutExt(key, value, false)
 }
 func (ck *Clerk) PutHash(key string, value string) string {
-  v := ck.PutExt(key, value, true)
-  return v
+	v := ck.PutExt(key, value, true)
+	return v
+}
+
+func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigx, _ := rand.Int(rand.Reader, max)
+	x := bigx.Int64()
+	return x
 }
--- src/pbservice/server.go
+++ src/pbservice/server.go
@@ -1,134 +1,481 @@
 package pbservice
 
-import "net"
-import "fmt"
-import "net/rpc"
-import "log"
-import "time"
-import "viewservice"
-import "os"
-import "syscall"
-import "math/rand"
-import "sync"
-
-//import "strconv"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"net/rpc"
+	"os"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+	"viewservice"
+)
 
 // Debugging
 const Debug = 0
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
-  if Debug > 0 {
-    n, err = fmt.Printf(format, a...)
-  }
-  return
+	if Debug > 0 {
+		n, err = fmt.Printf(format, a...)
+	}
+	return
 }
 
 type PBServer struct {
-  l net.Listener
-  dead bool // for testing
-  unreliable bool // for testing
-  me string
-  vs *viewservice.Clerk
-  done sync.WaitGroup
-  finish chan interface{}
-  // Your declarations here.
+	l          net.Listener
+	dead       bool // for testing
+	unreliable bool // for testing
+	me         string
+	vs         *viewservice.Clerk
+	done       sync.WaitGroup
+	finish     chan interface{}
+	// Your declarations here.
+	data           map[string]string
+	isPrimary      bool
+	isBackup       bool
+	records        map[int64]string
+	mu             sync.Mutex
+	curView        viewservice.View
+	dataBackupDone bool
 }
 
 func (pb *PBServer) Put(args *PutArgs, reply *PutReply) error {
-  // Your code here.
-  return nil
+	// Your code here.
+	pb.mu.Lock()
+	defer pb.mu.Unlock()
+	if !pb.isPrimary {
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if args.Viewnum != pb.curView.Viewnum {
+		// stale
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if !pb.dataBackupDone {
+		// Haven't backup
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if val, exist := pb.records[args.Id]; exist {
+		// Filter repeated requests
+		reply.Err = OK
+		reply.PreviousValue = val
+	} else {
+		// fmt.Printf("Backup %s\n", pb.curView.Backup)
+		if pb.curView.Backup != "" {
+			// fmt.Printf("Put\n")
+			ok := true
+			forwardReply := PutReply{}
+			forwardReply.Err = ""
+			forwardReply.PreviousValue = ""
+
+			// Debug
+			fmt.Printf("Put call PutForward, key %s, value %s, id %d\n", args.Key, args.Value, args.Id)
+			//
+
+			ok = call(pb.curView.Backup, "PBServer.PutForward", args, &forwardReply)
+			// Backup failed
+			if ok == false {
+				forwardReply.Err = ErrWrongServer
+			}
+			reply.Err = forwardReply.Err
+
+			// Debug
+			fmt.Printf("Put call PutForward have result, key %s, result %s, id %d, reply.Err %s\n", args.Key, args.Value, args.Id, reply.Err)
+			//
+			// fmt.Printf("Put %t, Err %s\n", ok, reply.Err)
+
+		} else {
+			reply.Err = OK
+		}
+
+		// Success, do put in local database
+		if reply.Err == OK {
+			if args.DoHash {
+				preVal, preEx := pb.data[args.Key]
+				if preEx == false {
+					preVal = ""
+				}
+				// strconv.FormatUint uses base = 10
+				pb.data[args.Key] = strconv.FormatUint(uint64(hash(preVal+args.Value)), 10)
+				reply.PreviousValue = preVal
+
+				// Save this request to record
+				pb.records[args.Id] = preVal
+
+				// Debug
+				fmt.Printf("Put Prevalue, key %s, value %s, id %d, preVal %s\n", args.Key, args.Value, args.Id, preVal)
+				//
+
+			} else {
+				// Don't modify reply.PreviousValue if not dohash
+
+				pb.data[args.Key] = args.Value
+				// Save this request to record
+				pb.records[args.Id] = ""
+			}
+
+			// Debug
+			fmt.Printf("Put local result, key %s, result %s, id %d, reply.Err %s\n", args.Key, pb.data[args.Key], args.Id, reply.Err)
+			//
+
+		}
+
+		// fmt.Printf("Put End Err %s\n", reply.Err)
+
+	}
+
+	return nil
+}
+
+func (pb *PBServer) PutForward(args *PutArgs, reply *PutReply) error {
+	// Your code here.
+	pb.mu.Lock()
+	defer pb.mu.Unlock()
+
+	if pb.isBackup == false {
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if args.Viewnum != pb.curView.Viewnum {
+		// stale
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	// Debug
+	fmt.Printf("PutForward get request, key %s, value %s, id %d\n", args.Key, args.Value, args.Id)
+	//
+
+	// Do not filter repeated requests
+	// if val, exist := pb.records[args.Id]; exist {
+	// 	// Filter repeated requests
+	// 	reply.PreviousValue = val
+	// } else {
+	// 	// Do put in local database
+
+	// }
+
+	if args.DoHash {
+		preVal, preEx := pb.data[args.Key]
+		if preEx == false {
+			preVal = ""
+		}
+
+		if v, e := pb.records[args.Id]; e {
+			preVal = v
+		}
+
+		// strconv.FormatUint uses base = 10
+		pb.data[args.Key] = strconv.FormatUint(uint64(hash(preVal+args.Value)), 10)
+		reply.PreviousValue = preVal
+
+		// Save this request to record
+		pb.records[args.Id] = preVal
+	} else {
+		// Don't modify reply.PreviousValue if not dohash
+
+		pb.data[args.Key] = args.Value
+		// Save this request to record
+		pb.records[args.Id] = ""
+	}
+
+	// Debug
+	fmt.Printf("PutForward local result, key %s, value %s, id %d, result %s, preValue %s\n", args.Key, args.Value, args.Id, pb.data[args.Key], reply.PreviousValue)
+	//
+
+	reply.Err = OK
+	return nil
 }
 
 func (pb *PBServer) Get(args *GetArgs, reply *GetReply) error {
-  // Your code here.
-  return nil
+	// Your code here.
+	pb.mu.Lock()
+	defer pb.mu.Unlock()
+
+	if !pb.isPrimary {
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if args.Viewnum != pb.curView.Viewnum {
+		// stale
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if !pb.dataBackupDone {
+		// Haven't backup
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if val, exist := pb.records[args.Id]; exist {
+		// Filter repeated requests
+		reply.Err = OK
+		reply.Value = val
+	} else {
+
+		if pb.curView.Backup != "" {
+			ok := true
+			forwardReply := GetReply{}
+			forwardReply.Err = ""
+			forwardReply.Value = ""
+
+			// Debug
+			fmt.Printf("Get call GetForward, key %s, id %d\n", args.Key, args.Id)
+			//
+
+			ok = call(pb.curView.Backup, "PBServer.GetForward", args, &forwardReply)
+
+			// Backup failed
+			if ok == false {
+				forwardReply.Err = ErrWrongServer
+			}
+			reply.Err = forwardReply.Err
+			reply.Value = forwardReply.Value
+
+			// Debug
+			fmt.Printf("Result : Get call GetForward, key %s, id %d, reply.Err %s. Result is %s\n", args.Key, args.Id, reply.Err, reply.Value)
+			//
+
+		} else {
+			reply.Err = OK
+		}
+
+		// TOCHECK: Do I need to save ErrNoKey to record? Now Do not save
+
+		// Success, do Get in local database
+		// if reply.Err == ErrNoKey, just return.
+		if reply.Err == OK {
+			// Key must exist
+			if v, e := pb.data[args.Key]; !e {
+				reply.Err = ErrNoKey
+				reply.Value = ""
+			} else {
+				reply.Value = v
+
+				//
+				// I don't check pb.data[args.Key] == forwardReply.Value here
+				// Because client's Put is called repeatly until Backup's Put successes.
+				// No need to consider client call GET before PUT successing,
+				// it is insoluble
+				//
+
+				// Save this request to record
+				pb.records[args.Id] = v
+			}
+
+		}
+	}
+	return nil
 }
 
+func (pb *PBServer) GetForward(args *GetArgs, reply *GetReply) error {
+	// Your code here.
+	pb.mu.Lock()
+	defer pb.mu.Unlock()
+
+	if pb.isBackup == false {
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if args.Viewnum != pb.curView.Viewnum {
+		// stale
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	// Debug
+	fmt.Printf("GetForward: key %s, id %d\n", args.Key, args.Id)
+	//
+
+	// if val, exist := pb.records[args.Id]; exist {
+	// 	// Filter repeated requests
+	// 	reply.Err = OK
+	// 	reply.Value = val
+	// } else {
+
+	// }
+
+	// Do GET in local database
+
+	// Check if has key
+	if v, e := pb.data[args.Key]; !e {
+		// TOCHECK: Do I need to save ErrNoKey to record? Now Do not save
+		reply.Err = ErrNoKey
+		reply.Value = ""
+	} else {
+		reply.Err = OK
+		reply.Value = v
+		// Save this request to record
+		pb.records[args.Id] = v
+	}
+
+	return nil
+}
+
+func (pb *PBServer) DataBackup(args *DataBackupArgs, reply *DataBackupReply) error {
+	pb.mu.Lock()
+	defer pb.mu.Unlock()
+
+	if !pb.isBackup {
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	if args.Viewnum != pb.curView.Viewnum {
+		// stale
+		reply.Err = ErrWrongServer
+		return nil
+	}
+
+	// filter out repeated dataBackup
+	if !pb.dataBackupDone {
+		pb.data = args.Database
+		pb.records = args.Record
+		pb.dataBackupDone = true
+	}
+
+	reply.Err = OK
+	return nil
+}
 
 // ping the viewserver periodically.
 func (pb *PBServer) tick() {
-  // Your code here.
-}
+	// Your code here.
+	pb.mu.Lock()
+	defer pb.mu.Unlock()
+	view, _ := pb.vs.Ping(pb.curView.Viewnum)
+	if view.Viewnum != pb.curView.Viewnum {
+		// new view
+		if view.Primary == pb.me {
+			pb.isPrimary = true
+			pb.isBackup = false
+			pb.dataBackupDone = view.Backup == ""
+		} else if view.Backup == pb.me {
+			pb.isPrimary = false
+			pb.isBackup = true
+			pb.dataBackupDone = false
+		} // otherwise, no action
+		pb.curView = view
+	}
 
+	// Check whether need dataBackup
+	if !pb.dataBackupDone && pb.isPrimary {
+		args := &DataBackupArgs{}
+		var reply DataBackupReply
+		args.Database = pb.data
+		args.Record = pb.records
+		args.Viewnum = pb.curView.Viewnum
+
+		// I have to use loop here. I have to make sure that dataBackup is successful
+		// after tick, because the next tick only Ping when dataBackupDone == true
+		for ok := false; !ok; {
+			ok = call(pb.curView.Backup, "PBServer.DataBackup", args, &reply)
+			if reply.Err != OK {
+				ok = false
+			}
+		}
+		pb.dataBackupDone = true
+	}
+
+}
 
 // tell the server to shut itself down.
 // please do not change this function.
 func (pb *PBServer) kill() {
-  pb.dead = true
-  pb.l.Close()
+	pb.dead = true
+	pb.l.Close()
 }
 
-
 func StartServer(vshost string, me string) *PBServer {
-  pb := new(PBServer)
-  pb.me = me
-  pb.vs = viewservice.MakeClerk(me, vshost)
-  pb.finish = make(chan interface{})
-  // Your pb.* initializations here.
+	pb := new(PBServer)
+	pb.me = me
+	pb.vs = viewservice.MakeClerk(me, vshost)
+	pb.finish = make(chan interface{})
+	// Your pb.* initializations here.
+	pb.data = make(map[string]string)
+	pb.isPrimary = false
+	pb.isBackup = false
+	pb.records = make(map[int64]string)
+	pb.curView.Viewnum = 0
+	pb.curView.Primary = ""
+	pb.curView.Backup = ""
+	pb.dataBackupDone = false
 
-  rpcs := rpc.NewServer()
-  rpcs.Register(pb)
+	rpcs := rpc.NewServer()
+	rpcs.Register(pb)
 
-  os.Remove(pb.me)
-  l, e := net.Listen("unix", pb.me);
-  if e != nil {
-    log.Fatal("listen error: ", e);
-  }
-  pb.l = l
+	os.Remove(pb.me)
+	l, e := net.Listen("unix", pb.me)
+	if e != nil {
+		log.Fatal("listen error: ", e)
+	}
+	pb.l = l
 
-  // please do not change any of the following code,
-  // or do anything to subvert it.
+	// please do not change any of the following code,
+	// or do anything to subvert it.
 
-  go func() {
-    for pb.dead == false {
-      conn, err := pb.l.Accept()
-      if err == nil && pb.dead == false {
-        if pb.unreliable && (rand.Int63() % 1000) < 100 {
-          // discard the request.
-          conn.Close()
-        } else if pb.unreliable && (rand.Int63() % 1000) < 200 {
-          // process the request but force discard of reply.
-          c1 := conn.(*net.UnixConn)
-          f, _ := c1.File()
-          err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
-          if err != nil {
-            fmt.Printf("shutdown: %v\n", err)
-          }
-          pb.done.Add(1)
-          go func() {
-            rpcs.ServeConn(conn)
-            pb.done.Done()
-          }()
-        } else {
-          pb.done.Add(1)
-          go func() {
-            rpcs.ServeConn(conn)
-            pb.done.Done()
-          }()
-        }
-      } else if err == nil {
-        conn.Close()
-      }
-      if err != nil && pb.dead == false {
-        fmt.Printf("PBServer(%v) accept: %v\n", me, err.Error())
-        pb.kill()
-      }
-    }
-    DPrintf("%s: wait until all request are done\n", pb.me)
-    pb.done.Wait() 
-    // If you have an additional thread in your solution, you could
-    // have it read to the finish channel to hear when to terminate.
-    close(pb.finish)
-  }()
+	go func() {
+		for pb.dead == false {
+			conn, err := pb.l.Accept()
+			if err == nil && pb.dead == false {
+				if pb.unreliable && (rand.Int63()%1000) < 100 {
+					// discard the request.
+					conn.Close()
+				} else if pb.unreliable && (rand.Int63()%1000) < 200 {
+					// process the request but force discard of reply.
+					c1 := conn.(*net.UnixConn)
+					f, _ := c1.File()
+					err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
+					if err != nil {
+						fmt.Printf("shutdown: %v\n", err)
+					}
+					pb.done.Add(1)
+					go func() {
+						rpcs.ServeConn(conn)
+						pb.done.Done()
+					}()
+				} else {
+					pb.done.Add(1)
+					go func() {
+						rpcs.ServeConn(conn)
+						pb.done.Done()
+					}()
+				}
+			} else if err == nil {
+				conn.Close()
+			}
+			if err != nil && pb.dead == false {
+				fmt.Printf("PBServer(%v) accept: %v\n", me, err.Error())
+				pb.kill()
+			}
+		}
+		DPrintf("%s: wait until all request are done\n", pb.me)
+		pb.done.Wait()
+		// If you have an additional thread in your solution, you could
+		// have it read to the finish channel to hear when to terminate.
+		close(pb.finish)
+	}()
 
-  pb.done.Add(1)
-  go func() {
-    for pb.dead == false {
-      pb.tick()
-      time.Sleep(viewservice.PingInterval)
-    }
-    pb.done.Done()
-  }()
+	pb.done.Add(1)
+	go func() {
+		for pb.dead == false {
+			pb.tick()
+			time.Sleep(viewservice.PingInterval)
+		}
+		pb.done.Done()
+	}()
 
-  return pb
+	return pb
 }
